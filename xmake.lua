db63036a9124ca453b7db95475131415505aafb5
SnowinterCat <SnowinterKat@gmail.com>
2026-02-03 12:06:40 +0800

    [update]xmake

--- xmake.lua
+++ xmake.lua
@@ -24,52 +24,49 @@ add_rules("plugin.compile_commands.autoupdate", {lsp = "clangd", outputdir = ".v
 
 -- 编译设置
 option("3rd_kind",     {showmenu = true, default = get_config("kind"), values = {"static", "shared"}})
+option("3rd_mode",     {showmenu = true, default = "release", values = {"release", "debug"}})
 option("outputdir",    {showmenu = true, default = path.join(os.projectdir(), "bin"), type = "string"})
 option("buildversion", {showmenu = true, default = 0, type = "number"})
 option("replace",      {showmenu = true, default = false, type = "boolean"}) -- export 三方库时是否强制替换
 
 includes("lua/check")
-check_macros("has_std_out_ptr",  "__cpp_lib_out_ptr",  {languages = stdcxx(), includes = "version"})
-check_macros("has_std_expected", "__cpp_lib_expected", {languages = stdcxx(), includes = "version"})
-check_macros("has_std_runtime_format", "__cpp_lib_format >= 202311L", {languages = stdcxx(), includes = "version"})
+check_macros("has_std_out_ptr",         "__cpp_lib_out_ptr",            {languages = stdcxx(), includes = "version"})
+check_macros("has_std_expected",        "__cpp_lib_expected",           {languages = stdcxx(), includes = "version"})
+check_macros("has_std_runtime_format",  "__cpp_lib_format >= 202311L",  {languages = stdcxx(), includes = "version"})
 
 -- 隐藏设置、隐藏目标、打包命令
 includes("lua/hideoptions.lua")
 includes("lua/hidetargets.lua")
 -- includes("lua/pack.lua")
 
--- some of the third-party use our own configurations
+-- some of the required libraries use our own repository
 add_repositories("myrepo 3rd", {rootdir = os.scriptdir()})
+-- detected libraries
+-- if not is_plat("android") then add_requires("vulkansdk") end
 -- header-only libraries
-if not has_config("has_std_out_ptr") then
-    add_requires("out_ptr")
-end
-if not has_config("has_std_expected") then
-    add_requires("tl_expected")
-end
--- add_requires("cxxopts", {version = "x.x.x"})
--- add_requires("vulkan-memory-allocator-hpp", {version = "3.1.0"})
-add_requires("tinygltf", {version = "x.x.x"})
+if not has_config("has_std_out_ptr") then add_requires("out_ptr") end
+if not has_config("has_std_expected") then add_requires("tl_expected") end
+add_requires("tinygltf", "eigen"--[[, "vulkan-memory-allocator-hpp", "cxxopts"]])
 -- normal libraries
-if not has_config("has_std_runtime_format") then
-    add_requires("fmt", {version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), header_only = false}})
-end
--- if not is_plat("android") then
---     add_requires("vulkansdk")
--- end
--- add_requires("libsdl3", {version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), x11 = true, wayland = true}})
--- add_requires("imgui", {version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared")}})
-add_requires("spdlog", {version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), header_only = false, fmt_external = not has_config("has_std_runtime_format"), std_format = has_config("has_std_runtime_format"), wchar = true, wchar_console = true}})
-add_requires("eigen", {version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared")}})
--- normal libraries' dependencies configurations
--- add_requireconfs("**.vulkan-memory-allocator", {override = true, version = "3.1.0"}) -- from vulkan-memory-allocator-hpp
--- add_requireconfs("**.vulkan-headers", {override = true, version = "1.3.296"})        -- from vulkan-memory-allocator
--- add_requireconfs("**.libxext", {system = true}) -- from libsdl3
--- add_requireconfs("**.libx11", {system = true})  -- from libsdl3
--- add_requireconfs("**.wayland", {system = true}) -- from libsdl3
-add_requireconfs("**.fmt", {override = true, version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), header_only = false}}) -- from spdlog
-add_requireconfs("**.nlohmann_json", {override = true, version = "x.x.x"}) -- from tinygltf
-add_requireconfs("**.stb", {override = true, version = "x.x.x"})           -- from tinygltf
+if not has_config("has_std_runtime_format") then add_requires("fmt") end
+add_requires("spdlog"--[[, "libsdl3", "imgui"]])
+-- configurations of required libraries
+add_requireconfs("**out_ptr",       {override = true, version = "x.x.x"})
+add_requireconfs("**tl_expected",   {override = true, version = "x.x.x"})
+add_requireconfs("**tinygltf",      {override = true, version = "x.x.x"})
+add_requireconfs("**eigen",         {override = true, version = "x.x.x"})
+-- add_requireconfs("**vulkan-memory-allocator-hpp", {override = true, version = "3.1.0"})
+add_requireconfs("**fmt",           {override = true, version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), debug = is_config("3rd_mode", "debug"), header_only = false}})
+add_requireconfs("**spdlog",        {override = true, version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), debug = is_config("3rd_mode", "debug"), header_only = false, fmt_external = not has_config("has_std_runtime_format"), std_format = has_config("has_std_runtime_format"), wchar = true, wchar_console = true}})
+-- add_requireconfs("**libsdl3",       {override = true, version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), debug = is_config("3rd_mode", "debug"), x11 = true, wayland = true}})
+-- add_requireconfs("**imgui",         {override = true, version = "x.x.x", configs = {shared = is_config("3rd_kind", "shared"), debug = is_config("3rd_mode", "debug")}})
+-- configurations of dependency libraries
+add_requireconfs("**nlohmann_json", {override = true, version = "x.x.x"})   -- from tinygltf
+add_requireconfs("**stb",           {override = true, version = "x.x.x"})   -- from tinygltf
+-- add_requireconfs("**vulkan-memory-allocator", {override = true, version = "3.1.0"}) -- from vulkan-memory-allocator-hpp
+-- add_requireconfs("**libxext", {system = true})  -- from libsdl3
+-- add_requireconfs("**libx11", {system = true})   -- from libsdl3
+-- add_requireconfs("**wayland", {system = true})  -- from libsdl3
 
 -- subdirectories
 includes("src/*/xmake.lua")
