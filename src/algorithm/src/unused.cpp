182b613a4b15182ddca5db5c575348f854e6bf72
SnowinterCat <SnowinterKat@github.com>
2026-02-05 07:31:33 +0800

    [fix]windows build

--- src/algorithm/src/unused.cpp
+++ src/algorithm/src/unused.cpp
@@ -1,1 +1,9 @@
 #include <gp/algorithm.hpp>
+
+GP_BEGIN
+GP_ALGORITHM_BEGIN
+
+void GP_ALGORITHM_API unused() {}
+
+GP_ALGORITHM_END
+GP_END
--- src/stl/src/unused.cpp
+++ src/stl/src/unused.cpp
@@ -1,1 +1,9 @@
 #include <gp/stl.hpp>
+
+GP_BEGIN
+GP_STL_BEGIN
+
+void GP_STL_API unused() {}
+
+GP_STL_END
+GP_END
